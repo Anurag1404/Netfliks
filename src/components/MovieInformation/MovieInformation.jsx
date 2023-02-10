bb1989c64f0af002bb497950698c4ff5d8f18698
Anurag1404 <singhanurag775@gmail.com>
2023-02-10 21:42:04 +0530

    Little Changes

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -77,20 +77,20 @@ const MovieInformation = () => {
       <Grid item sm={12} lg={4} className={classes.imageContainer}>
         <img
           className={classes.poster}
-          src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+          src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
           alt={data?.title}
         />
       </Grid>
       <Grid item container direction='column' lg={7}>
         <Typography variant='h3' align='center' gutterBottom>
-          {data?.title} ({data.release_date.split('-')[0]})
+          {data?.title} ({data?.release_date.split('-')[0]})
         </Typography>
         <Typography variant='h5' align='center' gutterBottom>
           {data?.tagline}
         </Typography>
         <Grid item className={classes.containerSpaceAround}>
           <Box display='flex' align='center'>
-            <Rating readOnly value={data.vote_average / 2} />
+            <Rating readOnly value={data?.vote_average / 2} />
             <Typography variant='subtitle1' gutterBottom style={{ marginLeft: '10px' }}>
               {data?.vote_average.toFixed(1)} / 10
             </Typography>
